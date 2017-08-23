2efe0d482583e7d165685882dfbe295c8ca0494d
Antonio Pimentel <antonio.pimentel@usach.cl>
2017-08-23 12:08:41 -0300

    commit4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,35 @@
 class PagesController < ApplicationController
   def batman
   end
+  def votos_batman
+
+  	a	= VotosBatman.new
+  	if params[:nombre].present?
+  		a.nombre = params[:nombre]#
+  	end
+  	if params[:email].present?
+  		a.email = params[:email]
+  	end
+  	if params[:nombre].present? || params[:email].present?
+  		a.save
+  	end
+  	redirect_to pages_batmanvssuperman_path notice: "Su voto ha sido registrado"
+  	@batman = VotosBatman.all	
+  end
+  def votos_superman
+
+  	a	= VotosSuperman.new
+  	if params[:nombre].present?
+  		a.nombre = params[:nombre]#
+  	end
+  	if params[:email].present?
+  		a.email = params[:email]
+  	end
+  	if params[:nombre].present? || params[:email].present?
+  		a.save
+  	end
+  	redirect_to pages_batmanvssuperman_path notice: "Su voto ha sido registrado"
+  	@batman = VotosSuperman.all	
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
-  root 'pages#batman-vs-superman'
-
+  root 'pages#batmanvssuperman'
+  post 'pages/votos_batman'
+  post 'pages/votos_superman'
+  get 'pages/batmanvssuperman'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
